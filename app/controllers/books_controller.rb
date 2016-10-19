af7bcfa35fc19494a386a0d00aba4b9ec77c4380
Plamen Jetchev <plamen.bodywork@gmail.com>
2016-10-19 12:40:47 -0400

    added delete,edit,back links, gem devise

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
 class BooksController < ApplicationController
-
+  before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
     @books = Book.all.order("created_at DESC")
+  end
+
+  def show
+
   end
 
   def new
@@ -17,9 +21,30 @@ class BooksController < ApplicationController
     end
   end
 
-  private
+  def edit
+  end
 
-  def book_params
-    params.require(:book).permit(:title, :description, :author)
+  def update
+    if @book.update(book_params)
+        redirect_to book_path(@book)
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to root_path
+
   end
 end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:title, :description, :author)
+    end
+
+    def find_book
+      @book = Book.find(params[:id])
+    end
