5883f11d29635f679690dfbbfacce925c608a9b1
Plamen Jetchev <plamen.bodywork@gmail.com>
2016-10-26 11:01:30 -0400

    Added review form and functionality, create, delete, edit.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :books
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
